a388d87c1fbbbf5ba6a4a4c0a139c08e9790a6fd
yasin aliyari <yasinaliyari30@gmail.com>
2025-09-07 15:23:57 +0330

    complete checkout view

--- app/views.py
+++ app/views.py
@@ -1,2 +1,11 @@
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+
+from app.models import Order, OrderItem
+
+
 def checkout(request, order_pk):
-    pass
+    order = get_object_or_404(Order, pk=order_pk)
+    items = OrderItem.objects.filter(order=order).select_related("product")
+    total_price = sum(item.product.price * item.quantity for item in items)
+    return JsonResponse({"total_price": format(total_price, ".6f")})
